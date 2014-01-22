54a2f28f9b2f153958b387388c59a558f5e6013a
s0dz <garrett.mctear@gmail.com>
2014-01-21 23:31:39 -0600

    Initial commit with base engine and window class.

---
+++ src/com/base/engine/MainComponent.java
@@ -0,0 +1,50 @@
+package com.base.engine;
+
+public class MainComponent
+{
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    public static final String TITLE = "3D Engine";
+    
+    public MainComponent()
+    {
+        
+    }
+    
+    public void start()
+    {
+        run();
+    }
+    
+    public void stop()
+    {
+        
+    }
+    
+    public void run()
+    {
+        while( !Window.isCloseRequested() )
+        {
+            render();
+        }
+    }
+    
+    public void render()
+    {
+        Window.render();
+    }
+    
+    public void cleanup()
+    {
+        
+    }
+    
+    public static void main(String[] args)
+    {
+        Window.createWindow( WIDTH, HEIGHT, TITLE );
+        
+        MainComponent game = new MainComponent();
+        
+        game.start();
+    }
+}
